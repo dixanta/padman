754d5802e0228f843ba75df529f72cb82057e84c
dixanta <dixanta@gmail.com>
2018-05-18 10:12:52 +0545

    spring project

--- src/main/java/com/heroku/web/AppConfigurer.java
+++ src/main/java/com/heroku/web/AppConfigurer.java
@@ -7,15 +7,21 @@ package com.heroku.web;
 
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import sun.misc.Contended;
 
 /**
  *
  * @author USER
  */
 @Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.heroku.web")
 public class AppConfigurer {
     @Bean
     public DataSource getDataSource(){
@@ -29,5 +35,10 @@ public class AppConfigurer {
     @Bean
     public JdbcTemplate getJdbcTemplate(){
         return new JdbcTemplate(getDataSource());
+    }
+    
+    @Bean
+    public InternalResourceViewResolver getViewResolver(){
+        return new InternalResourceViewResolver("/WEB-INF/views/",".jsp");
     }
 }
